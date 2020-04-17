bd45a0a4dc68e3b9d8205d651c9bfa935eb85a43
janseludo <jan.seludo@mydit.ie>
2020-04-17 14:46:10 +0100

    Space AR
    Commit initial

---
+++ GE2 Assignment/Assets/AttackController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AttackController : MonoBehaviour
+{
+    public GameObject defender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<StateMachine>().ChangeState(new AttackState());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+public class AttackState : State
+{
+    public override void Enter()
+    {
+        owner.GetComponent<Pursue>().target =
+            owner.GetComponent<AttackController>()
+            .defender.GetComponent<Boid>();
+        owner.GetComponent<Pursue>().enabled = true;
+    }
+
+    public override void Think()
+    {
+        if (Vector3.Distance(
+            owner.GetComponent<AttackController>()
+            .defender.transform.position,
+            owner.transform.position) < 10)
+        {
+            owner.ChangeState(new FleeState());
+        }
+    }
+
+    public override void Exit()
+    {
+        owner.GetComponent<Pursue>().enabled = false;
+    }
+}
+
+public class FleeState : State
+{
+    public override void Enter()
+    {
+        owner.GetComponent<Flee>().targetGameObject =
+            owner.GetComponent<AttackController>().defender;
+        owner.GetComponent<Flee>().enabled = true;
+    }
+
+    public override void Think()
+    {
+        if (Vector3.Distance(
+            owner.GetComponent<AttackController>()
+            .defender.transform.position,
+            owner.transform.position) > 30)
+        {
+            owner.ChangeState(new AttackState());
+        }
+    }
+    public override void Exit()
+    {
+        owner.GetComponent<Flee>().enabled = false;
+    }
+}
---
+++ GE2 Assignment/Assets/DefenderController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderController : MonoBehaviour
+{
+    public GameObject attacker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<StateMachine>().ChangeState(new PatrolState());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+class PatrolState : State
+{
+    public override void Enter()
+    {
+        owner.GetComponent<FollowPath>().enabled = true;
+    }
+
+    public override void Think()
+    {
+        if (Vector3.Distance(
+            owner.GetComponent<DefenderController>().attacker.transform.position,
+            owner.transform.position) < 10)
+        {
+            owner.ChangeState(new DefendState());
+        }
+    }
+
+    public override void Exit()
+    {
+        owner.GetComponent<FollowPath>().enabled = false;
+    }
+}
+
+public class DefendState : State
+{
+    public override void Enter()
+    {
+        owner.GetComponent<Pursue>().target = owner.GetComponent<DefenderController>().attacker.GetComponent<Boid>();
+        owner.GetComponent<Pursue>().enabled = true;
+    }
+
+    public override void Think()
+    {
+        if (Vector3.Distance(
+            owner.GetComponent<DefenderController>().attacker.transform.position,
+            owner.transform.position) > 30)
+        {
+            owner.ChangeState(new PatrolState());
+        }
+    }
+
+    public override void Exit()
+    {
+        owner.GetComponent<Pursue>().enabled = false;
+    }
+}
